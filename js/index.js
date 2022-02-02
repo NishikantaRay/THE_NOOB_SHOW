ed11e701270db3ff1a751082871e607baa18ab08
Nishikantaray <nishikantaray1@gmail.com>
2022-02-02 22:33:04 +0530

    fix navbar bug

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 const parallax = document.getElementById('parallax');
-window.addEventListener("scroll",function(){
-    let offset =window.pageYOffset;
-    parallax.style.backgroundPositionY = offset+0.7+"px";
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset + 0.7 + "px";
 });
-function test(){
-    console.log("test");
+function test() {
+  console.log("test");
 }
 
 
-  $(document).ready(function(){
-    setTimeout(function(){ test(); });
-  });
-  $(window).on('resize', function(){
-    setTimeout(function(){ test(); }, 500);
-  });
-  $(".navbar-toggler").click(function(){
-    setTimeout(function(){ test(); });
+$(document).ready(function () {
+  setTimeout(function () { test(); });
+});
+$(window).on('resize', function () {
+  setTimeout(function () { test(); }, 500);
+});
+$(".navbar-toggler").click(function () {
+  setTimeout(function () { test(); });
 });
 
