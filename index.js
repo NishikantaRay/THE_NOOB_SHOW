ad35c37ebb2aafd96a500c56957df9ff1cc31cb8
Nishikantaray <nishikantaray1@gmail.com>
2022-02-02 13:48:49 +0530

    footer modification

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const parallax = document.getElementById('parallax');
 window.addEventListener("scroll",function(){
     let offset =window.pageYOffset;
     parallax.style.backgroundPositionY = offset+0.7+"px";
-})
-
+});
+function test(){
+    console.log("test");
+}
 
 
   $(document).ready(function(){
