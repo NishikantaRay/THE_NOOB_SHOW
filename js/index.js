45ec7015dd889f571122daf643f02500a36aa4ec
Nishikantaray <nishikantaray1@gmail.com>
2022-03-12 23:33:55 +0530

    added

--- js/index.js
+++ js/index.js
@@ -18,3 +18,14 @@ $(".navbar-toggler").click(function () {
   setTimeout(function () { test(); });
 });
 
+    
+$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+$(window).on('load', function(){
+  setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+    $( "#loadingDiv" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      $( "#loadingDiv" ).remove(); //makes page more lightweight 
+  });  
+}
