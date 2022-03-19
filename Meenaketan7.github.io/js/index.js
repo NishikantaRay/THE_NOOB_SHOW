05d85627879845477ea5c9441ffe496b14dea8e2
Nishikantaray <nishikantaray1@gmail.com>
2022-03-20 00:15:06 +0530

    added

---
+++ Meenaketan7.github.io/js/index.js
@@ -0,0 +1,21 @@
+const parallax = document.getElementById('parallax');
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset + 0.7 + "px";
+});
+function test() {
+  console.log("test");
+}
+$(document).ready(function () {
+  setTimeout(function () { test(); });
+});
+$(window).on('resize', function () {
+  setTimeout(function () { test(); }, 500);
+});
+$(".navbar-toggler").click(function () {
+  setTimeout(function () { test(); });
+});
+setTimeout(function(){ 
+  document.getElementById('app').style['display'] = 'block';
+  document.getElementById('splash').style['display'] = 'none';
+}, 3000);
